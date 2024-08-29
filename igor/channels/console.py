85f288dd149dc6dceb81166e092e74bb33c580ab
Rasha Moumneh <rasha@rasha.dev>
2024-08-29 16:05:33 -0400

    Console Channel: refactor + graceful shutdown
    - The Console channel implements a graceful shutdown when the user enters 'q'
    - async_input is refactored to get the current event loop and use run_in_executor to run the blocking input() function in a separate thread and awaits the result

--- igor/channels/console.py
+++ igor/channels/console.py
@@ -1,10 +1,7 @@
 import asyncio
+import sys
 from igor.event import Event
-from igor.response import Response
 from igor.channels.base_channel import Channel
-from igor.logging_config import get_logger
-
-logger = get_logger(__name__)
 
 
 class Console(Channel):
@@ -16,16 +13,16 @@ class Console(Channel):
             try:
                 user_input = await self.async_input("> ")
                 if user_input.lower().startswith("igor"):
-                    event = self.channel_event_to_igor_event(user_input)
+                    event = Event(type="message", content=user_input, channel="console")
                     await self.hub.process_event(event)
                 elif user_input.lower() == "q":
-                    await self.stop_listening()
+                    self.hub.signal_shutdown()
                     print(f"{self.__class__.__name__} is shutting down")
                     break
             except asyncio.CancelledError:
                 break
             except Exception as e:
-                logger.debug(f"An error occurred with the console listening: {e}")
+                print(f"An error occurred: {e}")
 
     async def async_input(self, prompt):
         loop = asyncio.get_event_loop()
@@ -35,7 +32,7 @@ class Console(Channel):
         return Event(event_type="message", content=event, channel="console")
 
     async def send_response(self, event: Event, response: Response):
-        print(f"Igor: {response}")
+        print(f"Igor: {response.content}")
 
     async def stop_listening(self):
         self.hub.signal_shutdown()
