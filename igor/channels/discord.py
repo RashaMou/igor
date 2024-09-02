99485b40716971b33d7cd60d943df6a7ef710ce0
Rasha Moumneh <rasha@rasha.dev>
2024-09-02 17:27:31 -0400

    Add documentation and types to base_channel and base_reactor

--- igor/channels/discord.py
+++ igor/channels/discord.py
@@ -1,18 +1,17 @@
-import asyncio
 import os
 
+import asyncio
 from igor.channels.base_channel import Channel
-from igor.event import Event
 from igor.external.discord_api import DiscordAPI
 from dotenv import load_dotenv
+from igor.event import Event
 
 load_dotenv()
 
 class Discord(Channel):
     def __init__(self, hub):
         super().__init__(hub)
-        self.bot_token = os.getenv('DISCORD_BOT_TOKEN')
-        self.api = DiscordAPI(self.bot_token)
+        self.api = DiscordAPI(os.getenv('DISCORD_BOT_TOKEN'))
         self.running = False
 
     async def start_listening(self):
@@ -40,17 +39,17 @@ class Discord(Channel):
             try:
                 discord_event = await self.api.get_next_event()
                 if discord_event['d']['content'].lower().startswith("igor"):
-                    igor_event = self.igor_event_from_channel_event(discord_event)
+                    igor_event = self.channel_event_to_igor_event(discord_event)
                     await self.hub.process_event(igor_event)
             except Exception as e:
                 await asyncio.sleep(1)  # Avoid tight loop in case of recurring errors
 
-    def igor_event_from_channel_event(self, discord_event):
+    def channel_event_to_igor_event(self, channel_event):
         return Event(
             channel="discord",
             type="message",
-            content=discord_event['d']['content'],
-            discord_channel_id=discord_event['d']['channel_id']
+            content=channel_event['d']['content'],
+            discord_channel_id=channel_event['d']['channel_id']
         )
 
     async def send_response(self, event, response):
