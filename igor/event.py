60e2a3d8f00c3373e8a7b505eec04038c5414e52
Rasha Moumneh <rasha@rasha.dev>
2024-09-02 17:27:31 -0400

    Add documentation and types to base_channel and base_reactor

--- igor/event.py
+++ igor/event.py
@@ -1,6 +1,22 @@
+from dataclasses import dataclass, field
+from typing import Optional, Any
+
+@dataclass
 class Event:
-    def __init__(self, type: str, content: str, channel: str, discord_channel_id: str = None):
-        self.channel = channel
-        self.content = content
+    type: str
+    content: str
+    channel: str
+    additional_fields: dict(field(default_factory=dict))
+
+    def __init__(self, type: str, content: str, channel: str, **kwargs)
         self.type = type
-        self.discord_channel_id = discord_channel_id
+        self.content = content
+        self.channel = channel
+        self.additional_fields = kwargs
+
+    def __post_init__(self):
+        # handle any additonal keyword arguments
+        for key, value in self.additional_fields.items():
+            setatrr(self, key, value)
+        del self.additional_fields
+
