4f0a7b80c2313d168407667fbd4cb5caf0040451
Rasha Moumneh <rasha@rasha.dev>
2024-09-02 20:53:04 -0400

    base fixing

--- igor/event.py
+++ igor/event.py
@@ -1,22 +1,14 @@
-from dataclasses import dataclass, field
-from typing import Optional, Any
+from dataclasses import dataclass
 
 @dataclass
 class Event:
     type: str
     content: str
     channel: str
-    additional_fields: dict(field(default_factory=dict))
 
-    def __init__(self, type: str, content: str, channel: str, **kwargs)
+    def __init__(self, type: str, content: str, channel: str, **kwargs):
         self.type = type
         self.content = content
         self.channel = channel
-        self.additional_fields = kwargs
-
-    def __post_init__(self):
-        # handle any additonal keyword arguments
-        for key, value in self.additional_fields.items():
-            setatrr(self, key, value)
-        del self.additional_fields
-
+        for key, value in kwargs.items():
+            setattr(self, key, value)
